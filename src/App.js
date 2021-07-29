d97b5807a7272d2df43260bb0fe90f6697aa0210
samrahabb16 <samrahabb16@yahoo.com>
2021-07-29 17:44:37 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./App.css";
+
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+
+import Navbar from "./components/Navbar";
+
+import {  Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/rooms/" element={<Rooms />} />
+        <Route path="/rooms/:roomm" element={<SingleRoom />} />
+        <Route element={<Error />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
